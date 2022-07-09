c7d601a1f4510912a80dcda9c9ea4257776133e7
Alireza Lotfi <ar79lotfi@gmail.com>
2022-07-09 18:26:17 +0430

    Docker Compose Modified (api supported)

--- API/Database/Connection.go
+++ API/Database/Connection.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	_ "github.com/lib/pq"
 	"log"
+	"os"
 )
 
 type Postgresql struct {
@@ -12,13 +13,14 @@ type Postgresql struct {
 }
 
 func (dbClient *Postgresql) Init() error {
-	//DbUsername := os.Getenv("DB_USERNAME")
-	//DbPassword := os.Getenv("DB_PASSWORD")
-	//DbHost := os.Getenv("DB_HOST")
-	//DbPort := os.Getenv("DB_PORT")
-	//DbName := os.Getenv("DB_NAME")
+	DbUsername := os.Getenv("DB_USERNAME")
+	DbPassword := os.Getenv("DB_PASSWORD")
+	DbHost := os.Getenv("DB_HOST")
+	DbPort := os.Getenv("DB_PORT")
+	DbName := os.Getenv("DB_NAME")
 
-	connectionString := fmt.Sprintf("user=postgres dbname=postgres password=postgres host=localhost port=5432 sslmode=disable")
+	connectionString := fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%s sslmode=disable",
+		DbUsername, DbName, DbPassword, DbHost, DbPort)
 	var err error
 	dbClient.db, err = sql.Open("postgres", connectionString)
 	if err != nil {
---
+++ API/go.mod
@@ -0,0 +1,8 @@
+module API
+
+go 1.18
+
+require (
+	github.com/lib/pq v1.10.6
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d
+)
