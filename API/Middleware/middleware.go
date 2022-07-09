a533f039c4587dce19ed33336d973176ea5ffe64
MH2033 <mh.akbare@gmail.com>
2022-07-09 23:45:56 +0430

    Add middleware

---
+++ API/Middleware/middleware.go
@@ -0,0 +1,40 @@
+package middleware
+
+import (
+	"net/http"
+
+	"API/Infrastructure/auth"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware(authInterface auth.AuthInterface) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userId, err := auth.TokenValid(c.Request, authInterface)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"status": http.StatusUnauthorized,
+				"error":  err.Error(),
+			})
+			c.Abort()
+			return
+		}
+		c.Set("user_id", userId)
+		c.Next()
+	}
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
