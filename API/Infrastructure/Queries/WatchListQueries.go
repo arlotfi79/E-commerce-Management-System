f198ad6e01e01d287550ace05003918989f8eea5
Alireza Lotfi <ar79lotfi@gmail.com>
2022-07-11 02:18:13 +0430

    WatchList Queries Added

---
+++ API/Infrastructure/Queries/WatchListQueries.go
@@ -0,0 +1,41 @@
+package Queries
+
+import (
+	"API/Database"
+	"log"
+)
+
+type WatchListQuery struct {
+	dbClient *Database.Postgresql
+}
+
+func NewWatchListQuery(dbClient *Database.Postgresql) *WatchListQuery {
+	return &WatchListQuery{dbClient: dbClient}
+}
+
+func (watchListQuery *WatchListQuery) AddProductToWatchList(accountID uint64, productID uint64) error {
+	db := watchListQuery.dbClient.GetDB()
+
+	query, err := db.Prepare(`INSERT INTO WatchList (account_id, product_id)
+									VALUES ($1, $2)
+									ON CONFLICT 
+									    DO NOTHING `)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer query.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = query.Exec(accountID, productID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
