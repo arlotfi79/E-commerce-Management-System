ee7f85b1e42e3d8d147d9620ef88cd1f442d8a62
MH2033 <mh.akbare@gmail.com>
2022-07-11 14:03:28 +0430

    Passwordhash decrypt now accepts string

--- API/Infrastructure/PasswordSecurity/PasswordHash.go
+++ API/Infrastructure/PasswordSecurity/PasswordHash.go
@@ -7,7 +7,7 @@ func Encrypt(password string) ([]byte, error) {
 	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 }
 
-func Decrypt(hashedPassword, password string) error {
+func Decrypt(hashedPassword string, password string) error {
 
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 
