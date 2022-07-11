e277d60ffc411b6d3446a50367cc5dadecdf5175
MH2033 <mh.akbare@gmail.com>
2022-07-11 14:02:28 +0430

    Add handler for account

---
+++ API/Infrastructure/Handlers/AccountHandler.go
@@ -0,0 +1,67 @@
+package Handlers
+
+import (
+	"API/Communication/DataSignatures"
+	"API/Database"
+	q "API/Infrastructure/Queries"
+	pwd "API/Infrastructure/PasswordSecurity"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AccountHandler struct {
+	dbClient *Database.Postgresql
+}
+
+func (accountHandler *AccountHandler) NewAccountHandler(dbClient *Database.Postgresql) *AccountHandler {
+	return &AccountHandler{dbClient: dbClient}
+}
+
+func (accountHandler *AccountHandler) SignUpHandler (c *gin.Context) {
+	log.Println("New Request recevied")
+	var acc DataSignatures.Account
+	if err := c.ShouldBindJSON(&acc); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"err": "invalid json",
+		})
+		return
+	}
+	userq := q.NewUserQuery(accountHandler.dbClient)
+
+	//Check if user is duplicate
+	accounts, err := userq.GetUserByUname(acc.UserName)
+	if len(accounts) != 0 {
+		c.JSON(http.StatusForbidden, gin.H{
+			"err": "Duplicate username",
+		})
+		return
+	}
+
+	accounts, err = userq.GetUserByEmail(acc.Email)
+	if len(accounts) != 0 {
+		c.JSON(http.StatusForbidden, gin.H{
+			"err": "Duplicate email",
+		})
+		return
+	}
+
+	accounts, err = userq.GetUserByPhoneNumber(acc.PhoneNumber)
+	if len(accounts) != 0 {
+		c.JSON(http.StatusForbidden, gin.H{
+			"err": "Duplicate phoneNumber",
+		})
+		return
+	}
+
+	//Hashing password
+	hashedPass, _ := pwd.Encrypt(acc.Password)
+	acc.Password = string(hashedPass)
+	err = userq.CreateUser(&acc)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	
+}
