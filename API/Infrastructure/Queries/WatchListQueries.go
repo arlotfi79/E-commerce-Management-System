a207683366ce4a3ff3504a41ae35e7d5b31e3982
Alireza Lotfi <ar79lotfi@gmail.com>
2022-07-11 20:16:20 +0430

    Remove From WatchList added

--- API/Infrastructure/Queries/WatchListQueries.go
+++ API/Infrastructure/Queries/WatchListQueries.go
@@ -39,3 +39,28 @@ func (watchListQuery *WatchListQuery) AddProductToWatchList(accountID uint64, pr
 
 	return nil
 }
+
+func (watchListQuery *WatchListQuery) RemoveProductFromWatchList(accountID uint64, productID uint64) error {
+	db := watchListQuery.dbClient.GetDB()
+
+	query, err := db.Prepare(`DELETE FROM WatchList
+									WHERE account_id = $1 AND product_id = $2`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer query.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = query.Exec(accountID, productID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
