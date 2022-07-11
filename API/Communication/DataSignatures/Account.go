cec5e47afd3c75186aa1b2992d682fd96631de21
MH2033 <mh.akbare@gmail.com>
2022-07-11 22:55:01 +0430

    Fix field orders

--- API/Communication/DataSignatures/Account.go
+++ API/Communication/DataSignatures/Account.go
@@ -8,15 +8,28 @@ type PostAccount struct {
 	Name        string    `json:"name" binding:"required"`
 	LastName    string    `json:"lastName" binding:"required"`
 	UserName    string    `json:"username" binding:"required"`
-	Password    string    `json:"password" binding:"required"`
 	PhoneNumber string    `json:"phoneNumber" binding:"required"`
+	Password    string    `json:"password" binding:"required"`
 	Email       string    `json:"email" binding:"required"`
 	Gender      string    `json:"gender" binding:"required"`
 	BirthDate   time.Time `json:"birthDate" binding:"required"`
 	JoinDate    time.Time `json:"JoinDate" binding:"required"`
 }
 
+type SigninData struct {
+	UserName    string    `json:"username" binding:"required"`
+	Password    string    `json:"password" binding:"required"`
+}
+
 type GetAccount struct {
-	Id uint64 `json:"id" binding:"required"`
-	PostAccount
+	Id					uint64    `json:"id" binding:"required"`
+	Name        string    `json:"name" binding:"required"`
+	LastName    string    `json:"lastName" binding:"required"`
+	UserName    string    `json:"username" binding:"required"`
+	PhoneNumber string    `json:"phoneNumber" binding:"required"`
+	Password    string    `json:"password" binding:"required"`
+	Email       string    `json:"email" binding:"required"`
+	Gender      string    `json:"gender" binding:"required"`
+	BirthDate   time.Time `json:"birthDate" binding:"required"`
+	JoinDate    time.Time `json:"JoinDate" binding:"required"`
 }
