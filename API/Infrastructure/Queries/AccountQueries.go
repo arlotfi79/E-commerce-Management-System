887e6e6cf1a598ee106e2612260a892fa495bcc5
MH2033 <mh.akbare@gmail.com>
2022-07-11 14:02:47 +0430

    Add queris for getting user by email, phonenumber and username

--- API/Infrastructure/Queries/AccountQueries.go
+++ API/Infrastructure/Queries/AccountQueries.go
@@ -21,7 +21,7 @@ func (UserQuery *UserQuery) CreateUser(account *DataSignatures.Account) error {
 									VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 		return err
 	}
 
@@ -40,9 +40,104 @@ func (UserQuery *UserQuery) CreateUser(account *DataSignatures.Account) error {
 	)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 		return err
 	}
 
 	return nil
+}
+
+func (UserQuery *UserQuery) GetUserByUname(username string) ([]DataSignatures.Account, error) {
+	db := UserQuery.dbClient.GetDB()
+
+	query, err := db.Prepare(`SELECT * FROM Account WHERE user_name=$1`)
+
+	if err != nil {
+		log.Fatalln(err)
+		return nil, err
+	}
+
+	defer query.Close()
+	rows, err := query.Query(username)
+	if err != nil{
+		log.Fatalln(err)
+		return nil, err
+
+	}
+	var accounts []DataSignatures.Account
+	for rows.Next() {
+		var account DataSignatures.Account
+
+		err = rows.Scan(&account.Id, &account.Name, &account.LastName, &account.UserName, &account.PhoneNumber, &account.Password, &account.Email, &account.Gender, &account.BirthDate, &account.JoinDate)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		accounts = append(accounts, account)
+	}
+	return accounts, err
+}
+
+func (UserQuery *UserQuery) GetUserByEmail(email string) ([]DataSignatures.Account, error) {
+	db := UserQuery.dbClient.GetDB()
+
+	query, err := db.Prepare(`SELECT * FROM Account WHERE email=$1`)
+
+	if err != nil {
+		log.Fatalln(err)
+		return nil, err
+	}
+
+	defer query.Close()
+	rows, err := query.Query(email)
+	if err != nil{
+		log.Fatalln(err)
+		return nil, err
+
+	}
+	var accounts []DataSignatures.Account
+	for rows.Next() {
+		var account DataSignatures.Account
+
+		err = rows.Scan(&account.Id, &account.Name, &account.LastName, &account.UserName, &account.PhoneNumber, &account.Password, &account.Email, &account.Gender, &account.BirthDate, &account.JoinDate)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		accounts = append(accounts, account)
+	}
+	return accounts, err
+}
+
+func (UserQuery *UserQuery) GetUserByPhoneNumber(phoneNumber string) ([]DataSignatures.Account, error) {
+	db := UserQuery.dbClient.GetDB()
+
+	query, err := db.Prepare(`SELECT * FROM Account WHERE phone_number=$1`)
+
+	if err != nil {
+		log.Fatalln(err)
+		return nil,err
+	}
+
+	defer query.Close()
+	rows, err := query.Query(phoneNumber)
+	if err != nil{
+		log.Fatalln(err)
+		return nil, err
+	}
+	var accounts []DataSignatures.Account
+	for rows.Next() {
+		var account DataSignatures.Account
+
+		err = rows.Scan(&account.Id, &account.Name, &account.LastName, &account.UserName, &account.PhoneNumber, &account.Password, &account.Email, &account.Gender, &account.BirthDate, &account.JoinDate)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		accounts = append(accounts, account)
+	}
+	return accounts, err
 }
