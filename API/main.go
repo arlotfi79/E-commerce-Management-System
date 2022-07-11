4334086ae56575af7619a05af84a81cf72772e75
MH2033 <mh.akbare@gmail.com>
2022-07-11 22:54:46 +0430

    Connect to redis DB

--- API/main.go
+++ API/main.go
@@ -4,17 +4,32 @@ import (
 
 	"API/Database"
 	"API/Infrastructure/Handlers"
+	"API/Infrastructure/auth"
 
 	middleware "API/Middleware"
 
 	"log"
-
-
+	"os"
+	"context"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	var db Database.Postgresql
+	redisHost := os.Getenv("REDIS_HOST")
+	redisPort := os.Getenv("REDIS_PORT")
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+
+	var error error
+	redisService, error := auth.NewRedisDB(redisHost, redisPort, redisPassword, context.Background())
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	// authInt := auth.NewAuth()
+	tokenInt := auth.NewToken()
+
+
 	err := db.Init()
 	defer db.Close()
 
@@ -28,8 +43,10 @@ func main() {
 
 	var handlerObj Handlers.AccountHandler
 
-	accountHandler := handlerObj.NewAccountHandler(&db)
+	accountHandler := handlerObj.NewAccountHandler(&db, redisService.Auth, tokenInt)
 	router.POST("/signup", accountHandler.SignUpHandler)
+	router.POST("/signin", accountHandler.SigninHandler)
+
 	err = router.Run(":8081")
 	if err != nil {
 		return
