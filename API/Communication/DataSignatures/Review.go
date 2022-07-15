4eb12e281e303cd1cc3b964f544ee35ecaaf8c88
Alireza Lotfi <ar79lotfi@gmail.com>
2022-07-16 01:07:12 +0430

    Review signature changed

--- API/Communication/DataSignatures/Review.go
+++ API/Communication/DataSignatures/Review.go
@@ -2,13 +2,16 @@ package DataSignatures
 
 type ReviewWithVotes struct {
 	Id            uint64 `json:"id"`
+	Description   string `json:"description"`
 	Rating        uint64 `json:"rating"`
 	UpVoteCount   uint64 `json:"up_vote_count"`
 	DownVoteCount uint64 `json:"down_vote_count"`
+	RatingAVG     uint64 `json:"rating_avg"`
 }
 
 type PostReview struct {
-	ProductID string `json:"product_id"`
-	AccountID string `json:"account_id"`
-	Rating    uint64 `json:"rating"`
+	ProductID   string `json:"product_id"`
+	AccountID   string `json:"account_id"`
+	Description string `json:"description"`
+	Rating      uint64 `json:"rating"`
 }
